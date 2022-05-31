450f141ef0ab84cbf2b45a607e8e51083946574d
Arad Radfar <aradradfar@gmail.com>
2022-05-31 21:05:54 +0430

    first commit

---
+++ ApplePie/Game.swift
@@ -0,0 +1,33 @@
+//
+//  Game.swift
+//  ApplePie
+//
+//  Created by Arad Radfar on 6/31/1399 AP.
+//  Copyright © 1399 AP Arad Radfar. All rights reserved.
+//
+
+import Foundation
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
+    
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "_"
+            }
+        }
+        return guessedWord
+    }
+}
